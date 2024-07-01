65f6e8303318f50402b840349f86e84339866630
MaySinvani <maysinvani@gmail.com>
2024-07-01 13:44:54 +0300

    slide

--- server/index.js
+++ server/index.js
@@ -2,27 +2,32 @@ const express = require ('express');// crete new server and data base
 const mongoose=require ('mongoose');
 
 const app = express();// server
-mongoose.connect('mongodb://localhost:27017/shop')//our locl database 
+mongoose.connect('mongodb://localhost:27017/shop/items')//our locl database 
 
-const UserSchema=new mongoose.UserSchema({
+const ItemSchema=new mongoose.ItemSchema({
     name:string,
-    password:number
-})
+    price:number,
+    description: string
+});
 
-const UserModel=mongoose.model("users,UserSchema")
+const UserModel=mongoose.model("Item",ItemSchema);
 
-app.get("/getUsers",(req,res)=>{
-     res.json(UserModel.find({}).then(function(users) { 
-        res.json(users)
-        })).catch (function(err){ 
-            console.log(err)
-        })
-  
-     } )
-})
+app.get("/item",(req,res)=>{
+    const item = new Item({
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+    });
+    
+    item.save(function (err) {
+        if (err) {
+            res.redirect("/error");
+        } else {
+            res.redirect("/thank-you");
+        }
+    });
+    });
 
 app.use(express.static('public'));//using the files in public 
-
-
 
 app.listen(80);     
