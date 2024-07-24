60c3909ffda2c47a58cffebe5eb7954725665d99
SHAVITGH1 <shavitp21@gmail.com>
2024-07-24 21:49:54 +0300

    more changes

--- models/men.js
+++ models/men.js
@@ -1,8 +1,8 @@
 
 const menItems = [ // array of products
     {"id": '1', name: 'Product 1', price: 29.99, image: 'images/men/007.jpg', description: 'This is the first product' },
-    {"id": '2', name: 'Product 2', price: 19.99, image: 'images/men/brad.jpeg', description: 'This is the second product' },
-    {"id": '3', name: 'Product 3', price: 39.99, image: 'images/men/chris.jpg', description: 'This is the third product' },
+    {"id": '2', name: 'Product 2', price: 19.99, image: 'images/men/tomh.jpg', description: 'This is the second product' },
+    {"id": '3', name: 'Product 3', price: 39.99, image: 'images/men/ryan.jpg', description: 'This is the third product' },
     {"id": '4',name: 'Product 4', price: 49.99, image: 'images/men/harry.jpg', description: 'This is the fourth product' },
     {"id": '5',name: 'Product 5', price: 88.99, image: 'images/men/tom.jpg', description: 'This is the fifth product' }
 ]
