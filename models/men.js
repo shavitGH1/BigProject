3cb820b8e447403e8e1129dfbb61188e0f9c9135
SHAVITGH1 <shavitp21@gmail.com>
2024-07-27 02:41:49 +0300

    names

--- models/men.js
+++ models/men.js
@@ -1,10 +1,10 @@
 
 const menItems = [ // array of products
-    {"id": '1', name: 'Product 1', price: 29.99, image: 'images/men/007.jpg', description: 'This is the first product' },
-    {"id": '2', name: 'Product 2', price: 19.99, image: 'images/men/tomh.jpg', description: 'This is the second product' },
-    {"id": '3', name: 'Product 3', price: 39.99, image: 'images/men/ryan.jpg', description: 'This is the third product' },
-    {"id": '4',name: 'Product 4', price: 49.99, image: 'images/men/harry.jpg', description: 'This is the fourth product' },
-    {"id": '5',name: 'Product 5', price: 88.99, image: 'images/men/tom.jpg', description: 'This is the fifth product' }
+    {"id": '1', name: 'Product 1-007', price: 29.99, image: 'images/men/007.jpg', description: 'This is the first product' },
+    {"id": '2', name: 'Product 2-Tom', price: 19.99, image: 'images/men/tomh.jpg', description: 'This is the second product' },
+    {"id": '3', name: 'Product 3- ryan', price: 39.99, image: 'images/men/ryan.jpg', description: 'This is the third product' },
+    {"id": '4',name: 'Product 4-harry', price: 49.99, image: 'images/men/harry.jpg', description: 'This is the fourth product' },
+    {"id": '5',name: 'Product 5-Tomi', price: 88.99, image: 'images/men/tom.jpg', description: 'This is the fifth product' }
 ]
 
 function getAllMensItems(){//A function that returns all products
